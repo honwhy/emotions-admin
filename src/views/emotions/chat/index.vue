926c3d152af0a4793c5b9a01789eadc10aeae1c2
honwhy.wang <honwhy.wang@hotmail.com>
2024-01-16 23:58:40 +0800

    feat: add images

--- src/views/emotions/chat/index.vue
+++ src/views/emotions/chat/index.vue
@@ -19,23 +19,49 @@
       </el-col>
     </el-row>
     <el-row :gutter="12" class="step row">
-      <el-col v-for="item in flowPathList" :key="item" :span="3">
-        <div class="column-content">
+      <el-col v-for="(item, index) in flowPathList" :key="item" :span="3">
+        <div
+          class="column-content"
+          :class="{ active: activeIndex === index }"
+          @click="activeIndex = index"
+        >
           {{ item }}
         </div>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col :span="18">
+    <el-row :gutter="12">
+      <el-col :span="9" style="height: 550px">
+        <el-image
+          style="width: 100%; height: 100%"
+          :src="imageList[activeIndex]"
+          :zoom-rate="1.2"
+          :max-scale="7"
+          :min-scale="0.2"
+          :preview-src-list="[imageList[activeIndex]]"
+          fit="contain"
+        />
+      </el-col>
+      <el-col :span="9">
         <el-input
-          v-model="question"
-          :autosize="{ minRows: 5, maxRows: 10 }"
+          v-model="answer"
+          :autosize="{ minRows: 15, maxRows: 15 }"
           :input-style="{ fontSize: '18px' }"
           type="textarea"
-          placeholder="请输入"
+          placeholder="思考中..."
         />
-        <div style="margin: 20px 0; text-align: right">
+        <!-- <div style="margin: 20px 0; text-align: right">
+          <el-button type="primary" @click="onQuery" :disabled="question === ''" :loading="loading">提交</el-button>
+        </div> -->
+        <div class="dialog-input">
+          <el-input
+            v-model="question"
+            :autosize="{ minRows: 5, maxRows: 10 }"
+            :input-style="{ fontSize: '18px' }"
+            type="textarea"
+            placeholder="请输入"
+          />
           <el-button
+            class="send-button"
             type="primary"
             @click="onQuery"
             :disabled="question === ''"
@@ -43,15 +69,13 @@
             >提交</el-button
           >
         </div>
-        <el-input
-          v-model="answer"
-          :autosize="{ minRows: 5, maxRows: 10 }"
-          :input-style="{ fontSize: '18px' }"
-          type="textarea"
-          placeholder=""
-        />
       </el-col>
       <div class="emojis">
+        <span>
+          <el-icon :size="18" :color="'#939c9e'">
+            <QuestionFilled />
+          </el-icon>
+        </span>
         <span>{{ getScoreEmoji(1) }}</span>
         <span>{{ getScoreEmoji(2) }}</span>
         <span>{{ getScoreEmoji(3) }}</span>
@@ -67,6 +91,14 @@ import { getAnswer, getUserList, getFlowPathList } from "@/api/emotion/index";
 import type { QaAnswer } from "@/api/emotion/types";
 import { getScoreEmoji } from "../helper";
 import "twemoji-colr-font/twemoji.css";
+import img1 from "@/assets/images/phone_dial.jpg";
+import img2 from "@/assets/images/service.jpg";
+import img3 from "@/assets/images/pay.jpg";
+import img4 from "@/assets/images/usage.jpg";
+import img5 from "@/assets/images/claim.jpg";
+import img6 from "@/assets/images/comment.jpg";
+import { QuestionFilled } from "@element-plus/icons-vue";
+
 defineOptions({
   name: "ChatEmotions",
 });
@@ -100,8 +132,10 @@ function queryFlowPathList() {
     flowPathList.value = data;
   });
 }
+const activeIndex = ref(0);
+const imageList = [img1, img2, img3, img4, img5, img6];
 </script>
-<style scoped>
+<style scoped lang="scss">
 .emojis {
   font-family: "PingFang SC", "Microsoft YaHei", Twemoji, Arial, Helvetica,
     sans-serif;
@@ -150,6 +184,11 @@ function queryFlowPathList() {
     .column-content {
       font-weight: 500;
       color: #fff;
+      cursor: pointer;
+      background: #939c9e;
+    }
+
+    .active {
       background: #303435;
     }
 
@@ -225,5 +264,11 @@ function queryFlowPathList() {
       flex: 1;
     }
   }
+}
+
+.send-button {
+  position: relative;
+  bottom: 34px;
+  left: calc(100% - 62px);
 }
 </style>
